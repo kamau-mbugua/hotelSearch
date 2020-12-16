b91a5fc3cedfc99a0b7d210557d4ea46e42c45a9
kamau-mbugua <kamau_mbugua@yahoo.com>
2020-12-16 16:17:26 +0300

    Modified The app and designed it

---
+++ app/src/main/java/com/example/hotelsearch/AddHotelActivity.java
@@ -0,0 +1,23 @@
+package com.example.hotelsearch;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+public class AddHotelActivity extends AppCompatActivity {
+
+    ImageView ivBack;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_hotel);
+        ivBack= findViewById(R.id.ivBack);
+        ivBack.setOnClickListener(v -> {
+            startActivity(new Intent(getApplicationContext(), MainActivity.class));
+            finish();
+        });
+    }
+}
--- app/src/main/java/com/example/hotelsearch/MainActivity.java
+++ app/src/main/java/com/example/hotelsearch/MainActivity.java
@@ -1,14 +1,30 @@
 package com.example.hotelsearch;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.material.floatingactionbutton.ExtendedFloatingActionButton;
+
 public class MainActivity extends AppCompatActivity {
+
+    ExtendedFloatingActionButton extended_fab;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        extended_fab= findViewById(R.id.extended_fab);
+
+        extended_fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), AddHotelActivity.class));
+                finish();
+                return;
+            }
+        });
     }
 }
